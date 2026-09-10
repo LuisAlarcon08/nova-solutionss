a9d7cb18f5764e2bc980fd92748222110f5ccd77
LuisAlarcón08 <larmas-2023289@kinal.edu.gt>
2026-09-09 19:46:30 -0600

    security: se remueven credenciales de la conexion a base de datos

--- src/main/java/edu/enriques/nova/solutionss/config/Credentials.java
+++ src/main/java/edu/enriques/nova/solutionss/config/Credentials.java
@@ -3,7 +3,7 @@ package main.java.edu.enriques.nova.solutionss.config;
 public class Credentials {
     public static final String HOST = "localhost";
     public static final String PORT = "3306";
-    public static final String DB = "nova_solutions_db";
+    public static final String DB = "DB";
     public static final String USER = "root";
-    public static final String PASSWORD = "guichito2009$";
+    public static final String PASSWORD = "PASSWORD";
 }
